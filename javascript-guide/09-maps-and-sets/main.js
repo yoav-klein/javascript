4ad14748d930cb2ef6ceb7b35275b5846e5b4e5d
Yoav <yoavklein25@gmail.com>
2025-08-01 12:23:35 +0300

    Maps and sets

---
+++ javascript-guide/09-maps-and-sets/main.js
@@ -0,0 +1,44 @@
+
+// MAP OBJECT
+
+const sayings = new Map();
+sayings.set("dog", "woof");
+sayings.set("cat", "meow");
+sayings.set("elephant", "toot");
+sayings.size; // 3
+sayings.get("dog"); // woof
+sayings.get("fox"); // undefined
+sayings.has("bird"); // false
+sayings.delete("dog");
+sayings.has("dog"); // false
+
+for (const [key, value] of sayings) {
+  console.log(`${key} goes ${value}`);
+}
+// "cat goes meow"
+// "elephant goes toot"
+
+sayings.clear();
+sayings.size; // 0
+
+// SET OBJECTS
+const mySet = new Set();
+mySet.add(1);
+mySet.add("some text");
+mySet.add("foo");
+
+mySet.has(1); // true
+mySet.delete("foo");
+mySet.size; // 2
+
+for (const item of mySet) {
+  console.log(item);
+}
+// 1
+// "some text"
+
+// converting arrays and sets
+let arr1 = Array.from(mySet);
+
+let mySet2 = new Set([1, 2, 3, 4]);
+let arr2 = [...mySet2];
